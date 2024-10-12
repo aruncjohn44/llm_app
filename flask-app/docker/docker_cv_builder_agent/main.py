09fb68a7f953984d6b6a51d73dd6998b7ce2a38f
aruncjohn44 <aruncjohn@gmail.com>
2024-10-12 14:37:59 +0530

    Added html + css to pdf module i.eworking well

--- flask-app/docker/docker_cv_builder_agent/main.py
+++ flask-app/docker/docker_cv_builder_agent/main.py
@@ -1,26 +1,31 @@
-import asyncio
-from pyppeteer import launch
+# import asyncio
+# from pyppeteer import launch
+import os
 
-async def create_pdf_from_html(html, pdf_path):
-  browser = await launch()
-  page = await browser.newPage()
+os.chdir(r'flask-app\docker\docker_cv_builder_agent')
 
-  await page.setContent(html)
-  await page.pdf({'path': pdf_path, 'format': 'A4'})
-  await browser.close()
 
-# HTML content
-html = '''
-<html>
-  <head>
-      <title>PDF Example</title>
-  </head>
+# async def create_pdf_from_html(html, pdf_path):
+#   browser = await launch()
+#   page = await browser.newPage()
 
-  <body>
-      <h1>Hey, this will turn into a PDF!</h1>
-  </body>
-</html>
-'''
+#   await page.setContent(html)
+#   await page.pdf({'path': pdf_path, 'format': 'A4'})
+#   await browser.close()
 
-# Run create_pdf_from_html
-asyncio.get_event_loop().run_until_complete(create_pdf_from_html(html, 'from_html.pdf'))
+# if __name__ == "__main__":
+
+#   with open('template_cv.html', 'r') as file:
+#     html = file.read()
+#   asyncio.get_event_loop().run_until_complete(create_pdf_from_html(html, 'from_html.pdf'))
+
+import pdfkit
+
+# Path to the wkhtmltopdf executable (if it's not in your system's PATH)
+path_to_wkhtmltopdf = 'C:/Program Files/wkhtmltopdf/bin/wkhtmltopdf.exe'
+# Update this with your actual path
+pdfkit_config = pdfkit.configuration(wkhtmltopdf=path_to_wkhtmltopdf)
+
+# Convert HTML file with CSS to PDF
+pdfkit.from_file('template_cv.html', 'output.pdf', configuration=pdfkit_config)
+
